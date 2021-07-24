a828135b91f309dce34d19d222947d008b58c0e0
Atamyrat Abdyrahmanov <atamyrat@abdyrahmanov.com>
2021-07-25 02:34:27 +0300

    Dockerize the project

--- banking-service/config/index.js
+++ banking-service/config/index.js
@@ -2,7 +2,7 @@
 const dotenv = require('dotenv')
 const { join } = require('path')
 
-const result = dotenv.config({ path: join(__dirname, '../../.env') })
+const result = dotenv.config({ path: join(__dirname, '../.env') })
 if (result.error) {
   throw result.error
 }
--- banking-service/utils/bootstrap.js
+++ banking-service/utils/bootstrap.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const options = require('../config') 
 
 module.exports.connect = (url = options.DB_URL, opts = {}) => {
+  console.log('connected')
   return mongoose.connect(
     url,
     { ...opts, useNewUrlParser: true, useFindAndModify: false.valueOf, useUnifiedTopology: true }
