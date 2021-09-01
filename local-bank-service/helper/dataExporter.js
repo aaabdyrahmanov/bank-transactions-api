15a0de847a7b55b5cd34dc8411ce8a131f908167
Atamyrat Abdyrahmanov <atamyrat@abdyrahmanov.com>
2021-09-02 02:06:45 +0300

    Run the linter and fix errors

--- local-bank-service/helper/dataExporter.js
+++ local-bank-service/helper/dataExporter.js
@@ -1,7 +1,8 @@
+/* eslint-disable no-underscore-dangle */
 const { Transform } = require("json2csv");
-const { Readable } = require('stream');
+const { Readable } = require("stream");
 
-const { dateFormatter: formatDate } = require("./");
+const { dateFormatter: formatDate } = require(".");
 
 /**
  * Export the file as HTTP response attachment
@@ -10,11 +11,11 @@ const { dateFormatter: formatDate } = require("./");
  */
 module.exports = (res, file) => {
   const input = new Readable({
-    objectMode: true
+    objectMode: true,
   });
   input._read = () => {};
 
-  file.data.forEach((el, i) => {
+  file.data.forEach((el) => {
     input.push({
       id: el.id,
       _id: el._id,
@@ -22,19 +23,22 @@ module.exports = (res, file) => {
       counterpart_name: el.counterpart_name,
       counterpart_iban: el.counterpart_iban,
       date: formatDate(el.date),
-      createdAt: formatDate(el.createdAt)
-    });    
+      createdAt: formatDate(el.createdAt),
+    });
   });
   // Close the stream
-  input.push(null); 
+  input.push(null);
 
-  const transform = new Transform({
-    fields: file.fields
-  }, {
-    objectMode: true,
-    encoding: 'utf-8',
-    writableObjectMode: true
-  });
+  const transform = new Transform(
+    {
+      fields: file.fields,
+    },
+    {
+      objectMode: true,
+      encoding: "utf-8",
+      writableObjectMode: true,
+    }
+  );
 
   res.header("Content-Type", "text/csv");
   res.attachment(`${file.name}.${file.type}`);
