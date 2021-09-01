a010cab69b972320c6007e23d088dccf37059938
Atamyrat Abdyrahmanov <atamyrat@abdyrahmanov.com>
2021-09-02 02:12:11 +0300

    Remove no longer necessary fileWriter helper

--- local-bank-service/helper/index.js
+++ local-bank-service/helper/index.js
@@ -4,4 +4,3 @@
 module.exports.createAPICall = require("./apiCallCreator");
 module.exports.dateFormatter = require("./dateFormatter");
 module.exports.exportData = require("./dataExporter");
-module.exports.writeFile = require("./fileWriter");
