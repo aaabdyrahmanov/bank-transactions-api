cdc1c771862a08fc4ba597801f7a69ce3f281dc2
Atamyrat Abdyrahmanov <atamyrat@abdyrahmanov.com>
2021-09-02 01:11:27 +0300

    Ignore export data saving on memory with fs and export it with the help of streams

--- local-bank-service/helper/dataExporter.js
+++ local-bank-service/helper/dataExporter.js
@@ -1,6 +1,7 @@
-const { createReadStream } = require("fs");
 const { Transform } = require("json2csv");
-const writeFile = require("./fileWriter");
+const { Readable } = require('stream');
+
+const { dateFormatter: formatDate } = require("./");
 
 /**
  * Export the file as HTTP response attachment
@@ -8,24 +9,36 @@ const writeFile = require("./fileWriter");
  * @param {object} file - includes file config, fields and data
  */
 module.exports = (res, file) => {
-  // write the data as in expected file format
-  writeFile(file);
-
-  const transformOpts = { highWaterMark: 16384, encoding: "utf-8" };
-  const json2csv = new Transform({ fields: file.fields }, transformOpts);
-
-  const readStream = createReadStream(`${file.name}.${file.type}`, {
-    encoding: "utf8",
+  const input = new Readable({
+    objectMode: true
   });
-  readStream.on("data", (chunk) => {
-    console.log(`${chunk.length} : Data reeceived successfully!`);
+  input._read = () => {};
+
+  file.data.forEach((el, i) => {
+    input.push({
+      id: el.id,
+      _id: el._id,
+      amount: el.amount,
+      counterpart_name: el.counterpart_name,
+      counterpart_iban: el.counterpart_iban,
+      date: formatDate(el.date),
+      createdAt: formatDate(el.createdAt)
+    });    
+  });
+  // Close the stream
+  input.push(null); 
+
+  const transform = new Transform({
+    fields: file.fields
+  }, {
+    objectMode: true,
+    encoding: 'utf-8',
+    writableObjectMode: true
   });
 
-  // set response header
-  // attach file to response object
   res.header("Content-Type", "text/csv");
   res.attachment(`${file.name}.${file.type}`);
 
   // pipe the read stream to the response object
-  readStream.pipe(json2csv).pipe(res);
+  input.pipe(transform).pipe(res);
 };
