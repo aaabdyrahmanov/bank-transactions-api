82cf15c542890022a9151b17d9ef06243a2c232e
Atamyrat Abdyrahmanov <atamyrat@abdyrahmanov.com>
2021-09-17 01:15:10 +0300

    Fix typo in failure test case for channel publishing on amqp

--- local-bank-service/event-bus/__tests__/sync.publish.spec.js
+++ local-bank-service/event-bus/__tests__/sync.publish.spec.js
@@ -1,7 +1,7 @@
 const { publish } = require("..");
 
 describe("Message Broker Connection", () => {
-  test("AMQP Connection #1 - Should fail to connectn on amqp", async () =>
+  test("AMQP Connection #1 - Should fail to connect on amqp", async () =>
     publish().catch((err) => {
       expect(err).toBeDefined();
       expect(err.message.startsWith("Error Listening to URL ")).toBeTruthy();
